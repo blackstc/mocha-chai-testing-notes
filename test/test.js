60a4c26c592edb2d77bcf230ee6a391e842e2448
Blackstock <charlieblackstock@gmail.com>
2015-09-20 10:59:12 -0600

    added mocha-chai testing

--- test/test.js
+++ test/test.js
@@ -2,8 +2,34 @@ var expect = chai.expect;
 // var should = chai.should();
 
 // sample!
-describe('Compare Numbers', function() {
-  it('1 should equal 1', function() {
-    expect(1).to.equal(1);
+var expect = chai.expect;
+
+describe("Car tests", function () {
+  var car = new Car();
+
+  describe("Constructor", function () {
+    it("should have zero fuel", function () {
+      expect(car.fuel).to.equal(0);
+    });
+  });
+
+  describe("Driving tests", function () {
+    it("should return an error when attempting to drive without fuel", function () {
+      expect(car.drive()).to.equal("No fuel in car!");
+    });
+    it("should set fuel to max when fill is called without argument", function () {
+      car.fill();
+      expect(car.fuel).to.equal(car.max_fuel);
+    });
+    it("should set fuel according to driving", function () {
+      var initialFuel = car.fuel;
+      var milesDriven = 100;
+      var expectedFuel = initialFuel - (milesDriven / car.fuel_economy);
+      car.drive(milesDriven);
+      expect(car.fuel).to.equal(expectedFuel);
+    });
+    it("should return an error when attempting to drive more than fuel left in car", function () {
+      expect(car.drive(100000)).to.equal('Not enough fuel in car!');
+    });
   });
 });
